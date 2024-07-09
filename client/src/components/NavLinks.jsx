f2e9de11e7637dd2f2a2559fc531c9c7e66ec45d
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-07-09 18:31:03 +0300

    feat: :sparkles: structure the dashboard and create small and big sidebar with functionality and styling

---
+++ client/src/components/NavLinks.jsx
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom";
+import links from "../utils/links";
+import { useDashboardContext } from "../pages/DashboardLayout";
+
+const NavLinks = ({ isBigSidebar }) => {
+  const { toggleSidebar, user } = useDashboardContext();
+  return (
+    <div className="nav-links">
+      {links.map((link) => {
+        const { path, text, icon } = link;
+        return (
+          <NavLink
+            key={text}
+            className="nav-link"
+            to={path}
+            onClick={isBigSidebar ? null : toggleSidebar}
+          >
+            <span>{icon}</span>
+            {text}
+          </NavLink>
+        );
+      })}
+    </div>
+  );
+};
+
+export default NavLinks;
--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -1,9 +1,32 @@
+import { FaTimes } from "react-icons/fa";
 import Wrapper from "../assets/wrappers/SmallSidebar";
 import { useDashboardContext } from "../pages/DashboardLayout";
+import Logo from "./Logo";
+import { NavLink } from "react-router-dom";
+import links from "../utils/links";
+import NavLinks from "./NavLinks";
 
 const SmallSidebar = () => {
-  const data = useDashboardContext();
-  return <Wrapper>Small Sidebar</Wrapper>;
+  const { showSidebar, toggleSidebar } = useDashboardContext();
+  return (
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <button type="button" className="close-btn" onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default SmallSidebar;
