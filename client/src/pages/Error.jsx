97e0d5f015b4d87d68dc410b076681f08538010f
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-07-09 14:10:39 +0300

    feat: :sparkles: building error page with styling

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,13 +1,27 @@
-import React from "react";
 import { Link, useRouteError } from "react-router-dom";
+import Wrapper from "../assets/wrappers/ErrorPage";
+import img from "../assets/images/not-found.svg";
 
 const Error = () => {
   const error = useRouteError();
+
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <h3>Ohh! page not found</h3>
+          <p>We can't seem to find the page you're looking for</p>
+          <Link to="/dashboard">back home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
+
   return (
-    <div>
-      <h1>Error</h1>
-      <Link to="/">back home</Link>
-    </div>
+    <Wrapper>
+      <h3>Something went wrong</h3>
+    </Wrapper>
   );
 };
 
