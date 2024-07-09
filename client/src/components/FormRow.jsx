5d7c00307fd7b6d3ef9d4aff8f5a7c616359b07c
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-07-09 15:06:14 +0300

    feat: :memo: create a component for form row

---
+++ client/src/components/FormRow.jsx
@@ -0,0 +1,19 @@
+const FormRow = ({ type, name, labelText, defaultValue }) => {
+  return (
+    <div className="form-row">
+      <label htmlFor={name} className="form-label">
+        {name || labelText}
+      </label>
+      <input
+        type={type}
+        id={name}
+        className="form-input"
+        name={name}
+        defaultValue={defaultValue}
+        required
+      />
+    </div>
+  );
+};
+
+export default FormRow;
