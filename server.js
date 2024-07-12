76ec502e6c17e6d4e298a03a42637f4b5aa267e2
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-07-13 00:50:43 +0300

    feat: :sparkles: upload user image and update profile

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ import userRouter from "./routes/userRouter.js";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import path from "path";
+import cloudinary from "cloudinary";
 
 //middleware
 import errorHandlerMiddleware from "./middleware/errorhandlerMiddleware.js";
@@ -19,6 +20,12 @@ import errorHandlerMiddleware from "./middleware/errorhandlerMiddleware.js";
 import mongoose from "mongoose";
 import { authenticateUser } from "./middleware/authMiddelware.js";
 import cookieParser from "cookie-parser";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
