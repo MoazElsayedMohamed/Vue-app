f6db4fde5a5587aab4ef3800f58c36f4603e3540
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-07-13 15:00:32 +0300

    fix front end

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 app.use(cookieParser());
 app.use(express.json());
 
@@ -49,20 +49,9 @@ app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
 
-// // Get All jobs
-// app.get("/api/v1/jobs");
-
-// // Create Job
-// app.post("/api/v1/jobs");
-
-// // Get Single Job
-// app.get("/api/v1/jobs/:id");
-
-// // Edit Job
-// app.patch("/api/v1/jobs/:id");
-
-// // Delete Job
-// app.delete("/api/v1/jobs/:id");
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "/client/dist", "index.html"));
+});
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
